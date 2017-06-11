ede95a7fbc22ce2e4d99bbd336feb4e31af21a3f
e <2521781940@qq.com>
2017-06-11 20:26:07 +0800

    输入一个链表，反转链表后输出链表所以元素

---
+++ swordOffer/ReverseList.java
@@ -0,0 +1,31 @@
+package swordOffer;
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class ReverseList {
+	 public ListNode ReverseList(ListNode head) {
+			if (head == null || head.next == null) {
+				return head;
+			}
+			ListNode pre = head;
+			ListNode cur = head.next;
+			pre.next = null;
+			ListNode q = null;
+			
+			while (cur != null) {
+				
+				q = cur.next;
+				cur.next = pre;
+				pre = cur;
+				cur = q;
+				
+			}
+			return pre;
+	    }
+}
